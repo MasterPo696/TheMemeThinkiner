8673457c61d0f577e20184cec3cdcc242e42c5e4
TwoFace <Themasterpo69@gmail.com>
2025-02-11 13:52:27 +0300

    first commit

--- backend/app/collector/data_collector.py
+++ backend/app/collector/data_collector.py
@@ -1,22 +1,8 @@
 import asyncio
 import aiohttp
-import settings
+# import settings
 
 
-# print(settings.DEX_URL)  # Автоматически загрузит из .env
 
-class DataCollector():
-    async def __init__(self, url, key=None):
-        self.url = url
-        self.__key = key
-        
-    # Fetching the url and getting data (json)
-    async def fetch_data(self, session, url):
-        async with session.get(url) as response:
-            return await response.json()
-        
-    async def data_collector(self):
-        return
-        
 
 
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,4 @@
-import asyncio
+import asyncio 
+from app.collector import 
 if __name__ == '__main__':
     asyncio.run(data_collector())
